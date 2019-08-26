07ed5d64df6f35e16827f785ba44330f729e24b9
Mikhail Khoroshev <m.khoroshev@gmail.com>
2019-08-26 17:01:46 +0300

    Version 0.3

--- lesson03/hw03_easy.py
+++ lesson03/hw03_easy.py
@@ -1,20 +1,18 @@
-#########################################################
-#							#
-#   ЗАДАНИЕ СДЕЛАЮ ДОПОЛНИТЕЛЬН, НЕ БЫЛО ВРЕМЕНИ	#
-#   ПРОШУ ПРОВЕРИТЬ HARD ИЗ 2-ГО УРОКА 			#
-#   ВИДЕО С РАЗБОРОМ СПЕЦИАЛЬНО НЕ СМОТРЕЛ		#
-#   БЫЛО ИНЕРЕСНО СДЕЛАТЬ САМОМУ			#
-#							#
-#########################################################
-
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
-def my_round(number, ndigits):
-    pass
+def my_round(number, ndigits):             
+    new_number = number * 10**(ndigits+1)  
+    idx = int(new_number) % 10             
+    if idx >= 5:                           
+        new_number = new_number // 10 + 1  
+    else:                                  
+        new_number = new_number // 10      
+                                           
+    return new_number / 10**ndigits        
 
 
 print(my_round(2.1234567, 5))
@@ -29,9 +27,20 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    left_nbr = 0
+    right_nbr = 0
+    ticket_str = str(ticket_number)
+    idx = 0
+    count = int(len(ticket_str) / 2 - 1)
+    while idx <= count:
+        left_nbr += int(ticket_str[idx])
+        right_nbr += int(ticket_str[idx+count+1])
+        idx+=1
+
+    return "Билет счастливый" if left_nbr == right_nbr else "Билет НЕ счастливый"
 
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+print(lucky_ticket(123321))
 print(lucky_ticket(436751))
+
